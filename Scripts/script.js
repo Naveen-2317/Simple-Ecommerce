aa69311fe7926d6daad00317e039858292330930
Naveen Eerla <naveeneerla144@gmail.com>
2025-03-14 10:28:42 +0530

    cleared comments

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
-    // let urlMap = new Map([["Jewellery", "https://fakestoreapi.com/products/category/jewelery"],
-    // ["Women'sCloths", "https://fakestoreapi.com/products/category/women's clothing"],
-    // ["Men'sCloths", "https://fakestoreapi.com/products/category/men's clothing"],
-    // ["Electronics", "https://fakestoreapi.com/products/category/electronics"],
-    // ["All Products", "https://fakestoreapi.com/products"]]);
+    
     let urlMap = new Map([["Jewellery", "jewellery.html"],
         ["Women'sCloths", "womens.html"],
         ["Men'sCloths", "mens.html"],
@@ -18,31 +14,5 @@ $(document).ready(function () {
 
         }
 
-
-        // let categoryUrl = urlMap.get(requiredCat)
-        // console.log(categoryUrl);
-
-        // $.ajax({
-        //     url: categoryUrl,
-        //     method: "GET",
-        //     dataType: "JSON",
-        //     success: function (response) {
-        //         let productsGallery;
-        //         $("#gallery").empty();
-        //         response.forEach(function (product) {
-        //             $("#gallery").append(`
-        //                     <div class="image-item">
-        //                         <img src=${product.image} alt="Image" class="images">
-        //                         <div class="price">Price : ₹ ${product.price}</div>
-        //                         <div><button class="btn" data-price="${product.price}">Add to Bag</button></div>
-        //                     </div>
-        //                 `);
-        //         });
-        //     },
-        //     error: function (error) {
-        //         console.log(error);
-
-        //     }
-        // });
     });
 })
